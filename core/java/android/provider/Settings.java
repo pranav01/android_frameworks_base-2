42b5686f30e0ed26245e5fc3ec51894cfcb39ae7
Lars Greiss <kufikugel@googlemail.com>
2015-04-03 13:11:19 -0700

    Frameworks: Keyguard - Add user configurable shortcuts
    Change-Id: I55620f3835775792282ef8b9468ed0dd626c0beb

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -8789,6 +8789,21 @@ public final class Settings {
          */
         public static final String WAKELOCK_BLOCKING_LIST = "wakelock_blocking_list";
 
+        /**
+         * Defines the shortcuts to be shown on lockscreen
+         * Usage is like this: target:icon|target:icon|target:icon
+         * if :icon is not set, default application icon will be used
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS = "lockscreen_shortcuts";
+
+        /**
+         * Whether shorcuts open with normal or longpress
+         * @hide
+         */
+        public static final String LOCKSCREEN_SHORTCUTS_LONGPRESS =
+                "lockscreen_shortcuts_longpress";
+
     }
 
     /**
