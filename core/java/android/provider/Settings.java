146cc7f1d5fd6c9b6ab252fc00d4df2aa94f8a25
0xD34D <clark@scheffsblend.com>
2015-04-06 14:16:09 -0700

    [1/2] Screen Recorder
    http://forum.xda-developers.com/showthread.php?t=2547196
    
    Change-Id: I5bb81f909a44183fd8e011318f50a1b1e42c94e5
    
    Conflicts:
    
    	core/res/AndroidManifest.xml
    	core/res/res/values/cm_strings.xml
    	policy/src/com/android/internal/policy/impl/GlobalActions.java
    	policy/src/com/android/internal/policy/impl/PhoneWindowManager.java
    
    Conflicts:
    	core/java/android/provider/Settings.java
    	core/res/res/values/cmr_strings.xml
    	core/res/res/values/cr_config.xml
    	core/res/res/values/cr_symbols.xml
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3624,6 +3624,30 @@ public final class Settings {
          */
         public static final String VOLUME_KEYS_CONTROL_MEDIA_STREAM = "volume_keys_control_media_stream";
         /**
+         * Width and height of output vide expressed in WxH
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_OUTPUT_DIMENSIONS = "screen_recorder_output_dimensions";
+
+        /**
+         * Screen recorder framerate in bits per second
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_BITRATE = "screen_recorder_bitrate";
+
+        /**
+         * Whether power menu screen record is enabled
+         * @hide
+         */
+        public static final String POWER_MENU_SCREENRECORD_ENABLED = "power_menu_screenrecord_enabled";
+
+        /**
+         * Whether to include audio when recording a video
+         * @hide
+         */
+        public static final String SCREEN_RECORDER_RECORD_AUDIO = "screen_recorder_record_audio";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
