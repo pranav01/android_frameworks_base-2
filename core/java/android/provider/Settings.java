ef91b451027dd008bc19901b2550ce9e50a0650b
Muhammed Nazim <mnm9994u@gmail.com>
2015-04-10 12:22:32 -0700

    [3/3] frameworks/base: National Data Roaming
    Credits: CyanogenMod
    
    Linked commits:
            opt/telephony: https://gerrit.paranoidandroid.co/#/c/3655/
            services/Telephony: https://gerrit.paranoidandroid.co/#/c/3656/
    
    Change-Id: I0280d516067e1ef50f7321355cbee42009657d7e
    
    (cherry picked from commit 5f1e11e061209d2406ad088b428396ad9456546e)
    
    (cherry picked from commit 5f1e11e061209d2406ad088b428396ad9456546e)
    
    Conflicts:
    	core/java/android/provider/Settings.java

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3883,6 +3883,13 @@ public final class Settings {
         public static final String LOCK_SCREEN_ICON_COLOR = "lock_screen_icon_color";
 
         /**
+         * @hide
+         * Whether national data roaming should be used.
+         * @hide
+         */
+        public static final String MVNO_ROAMING = "mvno_roaming";
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
